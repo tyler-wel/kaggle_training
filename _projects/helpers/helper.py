d042fb8227bb393d5507c59e597ff44c99504c8f
tyl-welsh <tyler.welsh@seattleconsulting.co.jp>
2020-01-14 13:55:39 +0900

    feature engineering WIP

--- _projects/helpers/helper.py
+++ _projects/helpers/helper.py
@@ -1,2 +1,12 @@
-def hello():
-  return 'hello'
+def get_splits(dataframe, valid_fraction=0.1):
+    """
+    Splits a dataframe into train, validation, and test sets. 
+    Set the size of the validation and test sets with valid_fraction.
+        dataframe: Pandas dataframe to be split
+        valid_fraction: % to split data by (default 0.1)
+    """
+    valid_rows = int(len(dataframe) * valid_fraction)
+    train = dataframe[ : -valid_rows * 2 ]
+    valid = dataframe[ - valid_rows * 2 : -valid_rows]
+    test = dataframe[ -valid_rows : ]
+    return train, valid, test
