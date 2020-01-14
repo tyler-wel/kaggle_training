ed17d3bec163389d794fb111ab24c210eed61c03
tyler-wel <tylwelsh@gmail.com>
2020-01-14 22:46:27 +0900

    pipeline implementation

--- _projects/helpers/helper.py
+++ _projects/helpers/helper.py
@@ -10,3 +10,14 @@ def get_splits(dataframe, valid_fraction=0.1):
     valid = dataframe[ - valid_rows * 2 : -valid_rows]
     test = dataframe[ -valid_rows : ]
     return train, valid, test
+  
+from sklearn.pipeline import Pipeline
+# https://ramhiser.com/post/2018-03-25-feature-selection-with-scikit-learn-pipeline/
+class PipelineFS(Pipeline):
+  def fit(self, X, y=None, **fit_params):
+    """
+    Extension of Pipeline to support feature selection inside the pipeline.
+    """
+    super(PipelineFS, self).fit(X, y, **fit_params)
+    self.feature_importances_ = self.steps[-1][-1].feature_importances_
+    return self
