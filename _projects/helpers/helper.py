1801e0176da93c5144c16f7311101644d87f259f
tyl-welsh <tyler.welsh@seattleconsulting.co.jp>
2020-02-03 14:02:40 +0900

    started titanic v2

--- _projects/helpers/helper.py
+++ _projects/helpers/helper.py
@@ -13,6 +13,22 @@ def get_splits(dataframe, valid_fraction=0.1):
     test = dataframe[ -valid_rows : ]
     return train, valid, test
 
+from itertools import combinations
+def process_titanic(df, cat_col, num_col):
+  """
+    Process the Titanic dataset
+      df: Pandas dataframe
+      cat_col: categorical features
+      num_col: numerical features
+  """
+  # interactions
+  for comb in combinations(cat_col, 2):
+    new_feat = comb[0] + "_" + comb[1]
+    df[new_feat] = df[comb[0]].astype(str) + "_" + df[comb[1]].astype(str)
+  return(df)
+
+
+
 from sklearn.pipeline import Pipeline
 # https://ramhiser.com/post/2018-03-25-feature-selection-with-scikit-learn-pipeline/
 class PipelineFS(Pipeline):
@@ -24,3 +40,35 @@ class PipelineFS(Pipeline):
     super().fit(X, y, **fit_params)
     self.feature_importances_ = self.steps[-1][-1].feature_importances_
     return self
+  
+  
+# https://www.kaggle.com/baghern/a-deep-dive-into-sklearn-pipelines
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class TextSelector(BaseEstimator, TransformerMixin):
+    """
+    Transformer to select a single column from the data frame to perform additional transformations on
+    Use on text columns in the data
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[self.key]
+    
+class NumberSelector(BaseEstimator, TransformerMixin):
+    """
+    Transformer to select a single column from the data frame to perform additional transformations on
+    Use on numeric columns in the data
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[[self.key]]
